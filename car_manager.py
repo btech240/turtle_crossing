b4b0101b4c074e2fc455c9d170da2d4447e542e3
btech240 <brenttlong@gmail.com>
2024-01-15 17:01:41 -0500

    Added all starter files. Added logic to move turtle

---
+++ car_manager.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+COLORS = ["red", "green", "yellow", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    pass
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import time
 from turtle import Screen
 
+from car_manager import CarManager
+from player import Player
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-game_is_on = True
+player = Player()
+screen.listen()
 
+screen.listen()
+
+# Left paddle control
+screen.onkey(player.go_up, "Up")
+
+
+game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
+# Prevent exit until click
+screen.exitonclick()
